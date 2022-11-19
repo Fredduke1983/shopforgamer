c7db01f6e846ec11c2ffea40dc0a353e2cd3c275
Fredduke1983 <fredduke1983@gmail.com>
2022-11-19 22:35:08 +0000

    Deploying to gh-pages from @ Fredduke1983/shopforgamer@560ab5f88adc0fad50b4d698f50953b47ae48474 🚀

---
+++ index.80c428ef.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var a=!0===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.80c428ef.js.map
---
+++ index.ff10d2c4.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const a=!0===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}));
+//# sourceMappingURL=index.ff10d2c4.js.map
