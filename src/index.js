7e14fe01823b49cc3c7fc078bdd152e307f68d45
Nechai_Ihor <fredduke1983@gmail.com>
2022-11-23 00:17:21 +0200

    up gallery

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-const menuBtnRef = document.querySelector("[data-menu-button]");
-const mobileMenuRef = document.querySelector("[data-menu]");
+const menuBtnRef = document.querySelector('[data-menu-button]');
+const mobileMenuRef = document.querySelector('[data-menu]');
 
-menuBtnRef.addEventListener("click", () => {
-	const expanded =
-		menuBtnRef.getAttribute("aria-expanded") === true || false;
-	
-	menuBtnRef.classList.toggle("is-open");
-	menuBtnRef.setAttribute("aria-expanded", !expanded);
+menuBtnRef.addEventListener('click', () => {
+  const expanded = menuBtnRef.getAttribute('aria-expanded') === true || false;
 
-	mobileMenuRef.classList.toggle("is-open");
+  menuBtnRef.classList.toggle('is-open');
+  menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+  mobileMenuRef.classList.toggle('is-open');
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
